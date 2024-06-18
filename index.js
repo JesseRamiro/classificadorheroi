38e94cf166ef3bd2c14f1c2878e0131b2358b1a5
Jesse Ramiro <jesseramiro@gmail.com>
2024-06-18 02:39:23 -0300

    versão final

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 // Desafio Classificador Heroi 
 
-let nome = "Black Panther"
+const nome = "Black Panther"
 let teste= true
 let xp = 11000
+let nivel;
 let msg1 = "O Herói de nome "
 let msg2 = " está no nível " 
 console.log("CLASSIFICADOR DE HEROI")
 
 switch (teste) {
    case xp <= 1000 :
-     console.log(msg1 + nome + msg2 + "ferro");
+     nivel = "ferro";
      break;
 
    case xp >=1001 && xp<=2000:
-     console.log(msg1 + nome + msg2 + "bronze");
+     nivel = "bronze";
      break;
 
    case xp >=2001 && xp<=5000:
-     console.log(msg1 + nome + msg2 + "prata")
+     nivel = "prata"
      break;
 
    case xp >=5001 && xp<=7000:
-     console.log(msg1 + nome + msg2 + "ouro")
+     nivel = "ouro"
      break;
 
    case xp>=7001 && xp<=8000:
-     console.log(msg1 + nome + msg2 + "platina")
+     nivel = "platina"
      break;
 
    case xp>=8001 && xp<=9000:
-     console.log(msg1 + nome + msg2 + "ascendente")
+     nivel = "ascendente"
      break;
 
    case xp>=9001 && xp<=10000:
-     console.log(msg1 + nome + msg2 + "imortal")
+     nivel = "imortal"
      break;
 
    case xp>10001:
-     console.log(msg1 + nome + msg2 + "radiante")
+     nivel = "radiante"
      break;
-
 }
-
+console.log(msg1+nome+msg2+nivel)
